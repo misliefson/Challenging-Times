479d1dcee1bd41fe5cc270f964f4fd661dc9a3d1
Matthew Isliefson <misliefson@gmail.com>
2017-10-29 23:17:15 +0000

    Part One Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  resources :pages
+
+  get '/' => 'pages#home', as: 'home'
+  get '/about_us' => 'pages#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
